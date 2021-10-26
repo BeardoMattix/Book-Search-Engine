050e44e6b03ee5146a53a995d593ecd158acf894
Beardo Mattix <christopher.mattix@gmail.com>
2021-10-26 14:26:42 -0400

    resolvers should be good now

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -30,11 +30,44 @@ const resolvers = {
 
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
-
+      // This check to see if the user has valid credintials (email and password) and throws an error if they do not exist.
       if (!user) {
         throw new AuthenticationError("Invalid credentials");
       }
       const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Invalid credentials");
+      }
+      //This will immediatel sign the JWT and allow the user to be logged in.
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    //Context allows us to get the logged in use without having to do aa search.
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: args.input } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You must be logged in to do that!");
+    },
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+
+          { $pull: { savedBooks: { bookId: args.bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to log in!");
     },
   },
 };
+
+module.exports = resolvers;
