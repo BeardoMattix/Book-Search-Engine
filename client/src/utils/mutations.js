9a99acc7059e78e3ffb060140ffee0367802d00a
Beardo Mattix <christopher.mattix@gmail.com>
2021-10-27 10:28:58 -0400

    added the remove book mutation and it is basically the same as the save book mutation just change the input to remove book by id

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -44,4 +44,22 @@ export const SAVE_BOOK = gql`
   }
 `;
 
-export const REMOVE_BOOK
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      email
+      savedBooks {
+        #_id
+        bookId
+        authors
+        link
+        image
+        description
+        title
+      }
+    }
+  }
+`;
