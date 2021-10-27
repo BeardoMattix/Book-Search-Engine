99463151a06b8fd2d473bf4b87047e3e8b58c153
Beardo Mattix <christopher.mattix@gmail.com>
2021-10-27 10:31:56 -0400

    added comments to the mutations file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,6 @@
 import { gql } from "@apollo/client";
 
+//This logs in the user based on email and password.
 export const loginUser = gql`
   mutation loginUser($email: String!, $password: String!) {
     loginUser(email: $email, password: $password) {
@@ -12,6 +13,7 @@ export const loginUser = gql`
   }
 `;
 
+// Adds a new user. They enter a username, email, and password, which will be validated with JWT to log them in.
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUSer(username: $username, email: $email, password: $password) {
@@ -23,7 +25,7 @@ export const ADD_USER = gql`
     }
   }
 `;
-
+//This takes in the input and saves the book.
 export const SAVE_BOOK = gql`
   mutation saveBook($input: savedBook!) {
     saveBook(input: $input) {
@@ -43,7 +45,7 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
-
+// This will remove the book based on its ID.
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
